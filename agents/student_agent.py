b17b0e03f4d9c38688b9fbf59101d8d68f7a5d43
Victor Micha <victor.micha@mail.mcgill.ca>
2023-12-01 19:06:16 -0500

    Basic structure of student agent using heuristic search

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -7,6 +7,40 @@ from copy import deepcopy
 import time
 
 
+
+"""
+We have chosen to implement a heuristic search similar to A* without guarenteeing that h is optimal
+
+we want to minimize f(n) = g(n) + h(n) to find shortest path in graph of game states
+g(n) -> self.num_steps_taken
+h(n) is defined below
+
+game state is a 3-tuple: chess_board, my_pos, adv_pos
+
+"""
+
+# h is the heuristic function
+# chess_board is to know where the walls are
+# my_pos and adv_pos to determine where players are on board
+# need to return a low number when game state is good for my player
+# need to return a high number when game state is bad for my player
+# returns a 0 if this game state is a winning game state for this player (game over)
+# returns a 100 if this game state is a losing game state for this player (game over)
+# returns a number between 0 and 100
+def h(student_agent, chess_board, my_pos, adv_pos):
+    my_r, my_c = my_pos
+
+    #if chess_board[my_r, my_c, student_agent.dir_map["u"]] and chess_board[my_r, my_c, student_agent.dir_map["u"]]
+
+    return 0#TODO
+
+
+def g(student_agent):
+    return student_agent.num_steps_taken
+
+def f(student_agent, chess_board, my_pos, adv_pos):
+    return g(student_agent) + h(student_agent, chess_board, my_pos, adv_pos)
+
 @register_agent("student_agent")
 class StudentAgent(Agent):
     """
@@ -23,12 +57,15 @@ class StudentAgent(Agent):
             "d": 2,
             "l": 3,
         }
+        self.num_steps_taken = 0#num times step function is called, to determine g(n)
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
         Implement the step function of your agent here.
         You can use the following variables to access the chess board:
         - chess_board: a numpy array of shape (x_max, y_max, 4)
+            (_ , _, self.dir_map["u"]) or self.dir_map["l"]...
+            chess_board(_, _, _) = True means wall (there are walls on the borders so agents dont go off-screen)
         - my_pos: a tuple of (x, y)
         - adv_pos: a tuple of (x, y)
         - max_step: an integer
@@ -44,9 +81,73 @@ class StudentAgent(Agent):
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
         start_time = time.time()
+
+        #TODO 
+        #need to try every possible sequence of hops < max_step, while keeping track of lowest f(n)
+        
+        #need to call f(self, chess_board, my_pos, adv_pos)
+        #TODO  pick move that has the lowest f(n)
+        
+
+
+        """
+        need to try every possible sequence of hops < max_step
+        for example if max_step = 3, need to check all possible moves for doing 0, 1, 2, and 3 hops
+        num hops taken <= max_step 
+        num hops taken  : max  num possible moves (without counting walls to put down)
+        0 : 1 -> 0 hops taken means only 1 possible move (dont move)
+        0,1 : 5  -> making 0 hop then 1 hop, 5 total move options (u, d, l, r, no movement)
+        0,1,2 : 13
+        0,1,2,3 : 25
+        0,1,2,3,4 : 41
+        0,1,2,3,4,5 : 61 -> for num hops = 0, 1, 2, 3, 4, then 5, 61 total move options (r,c) to go to
+        ...
+       
+         
+        0,1 : 2(2)+1  # +1 for no movement
+        0,1,2 : 2(4)+2(2)+1
+        0,1,2,3 : 2(6)+2(4)+2(2)+1
+        0,1,2,3,4 : 2(8)+2(6)+2(4)+2(2)+1
+        0,1,2,3,4,5 : 2(10)+2(8)+2(6)+2(4)+2(2)+1
+                                
+
+        0,1,...,max_step : 4*(1+2+3+...+max_step)+1
+
+
+         
+        # total number of moves counting wall to put down <= max num possible moves (without putting wall down) * 4 (biggest case have 4 options to put wall down: u,d,l,r)
+        total num moves when trying 0 hop, 1 hop, ..., max_step hops and placing a wall = 4*(4*(1+2+3+...+max_step)+1)
+        
+        = 4*(4*(max_step*(max_step+1)/2)+1)
+        = 4*(2*max_step*(max_step+1)+1)
+        = 8*max_step*(max_step+1)+4
+        = O(max_step^2)
+        
+        for each move, we need to get f(n) and keep track of lowest ones (break tie with randomness)
+        
+        for max_step = 5, total number of moves (including putting wall down) = 61*4=244    
+
+    
+        ---------pseudocode------------
+        need to try hopping numHops=0...max_step inclusive.
+        for each numHops gather f(n) of each gamestate (adding walls u,d,l,r if possible)
+        keep track of lowest f(n), what r and c, and where to put wall u or d or l or r
+        
+        example: when numHops = 0 -> no hops are done so we have 4 options of where to put wall (u,d,l,r), call f(n) for each of the 4 wall placement and keep track of smallest f(n) 
+        when max_step = 1 -> try numHops =0, follow instruction above. Then try numHops = 1. Hopping 1 time gives us 4 possible options: going up, down, left, or right. For each of these 4 moves, we have 4 options of wall placement (u,d,l,r). So for 1 numHop we have 4*4=16 total options without counting options of 0 numHops
+        --------------------------------
+        
+
+        """
+        
         time_taken = time.time() - start_time
         
-        print("My AI's turn took ", time_taken, "seconds.")
+        print("Victor's AI's turn took ", time_taken, "seconds.")
+        print(">>>>>>>>>>>>>>>>>>>>>>>>"+str(max_step))
+        #print(">>>>>>>>>>>>>"+str(chess_board[0,0, self.dir_map["l"]]))
+
+        self.num_steps_taken += 1#step function was called, so increment by one
+        
 
         # dummy return
         return my_pos, self.dir_map["u"]
