1f3fde2523ff2a148b151fb7060c19299eed797e
Pratham Bansal <pratham.bansal@mail.mcgill.ca>
2023-12-05 20:12:06 -0500

    Improve h function with wall_count

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -39,14 +39,16 @@ def h(student_agent, chess_board, new_pos, adv_pos, wall_dir, starting_pos):
     total_turns_in_match = chess_board.shape[0] * chess_board.shape[1]  
     turns_played = student_agent.num_steps_taken
     game_phase = 'late' if turns_played > total_turns_in_match / 2 else 'early'
+    
 
+    # NOTE: These constants resulted in 100% victory.
     # Set weights dynamically based on the game phase
     if game_phase == 'early':
         # Early game: prioritize exploration and control
-        c1, c2, c3 = 1, 5, -0.5  
+        c1, c2, c3, c4 = 1, 5, -0.5 ,20
     else:
         # Late game: prioritize securing space and trapping the opponent
-        c1, c2, c3 = 1, 10, -1   
+        c1, c2, c3,c4 = 1, 10, -1 , 50  
 
     #CURRENTLTY AGENT IS AT STARTING_POS
     #THIS H CALL IS TO DECIDE IF MOVING TO NEW_POS AND ADDING A WALL AT WALL_DIR A GOOD DECISION
@@ -59,9 +61,7 @@ def h(student_agent, chess_board, new_pos, adv_pos, wall_dir, starting_pos):
     # h = c1 * my_r + c2 * my_c + c3 * otherVariable + ...
     # could maybe use least squares line of best fit method to find the best constants c1, c2 ...
     # no more heuristic_factor -> dont need to scale h, negative values work too (negative means game state is super favored)
-     
-    #c1, c2 = 1, 10
-    #c6, c7, c8, c9, c10 = 5, 0, 0, 0 ,0    
+        
     #for now trying linear combination, could potentially try other functions
     
     # Distance Between Players
@@ -70,13 +70,18 @@ def h(student_agent, chess_board, new_pos, adv_pos, wall_dir, starting_pos):
     
     # Board Control and Division
     controlled_area = flood_fill(chess_board, new_pos)
-    c3 = -0.5  # Negative weight, as more controlled area is better
+    #c3 = -0.5  # Negative weight, as more controlled area is better
+    
+    # Check if the agent is surrounded by three walls
+    wall_count = sum(chess_board[new_r, new_c])
+    trapped_penalty = c4 * (1 if wall_count >= 3 else 0)
 
     #variables used for h should be vars that change within the step function. numWalls and adv_pos dont change
     #my_pos and where the wall was just added are the only things that will change within the step function
     # h(my_pos, wall_dir)
     
-    return c1*distanceBetweenMeAndAdv + c2*numWalls(chess_board) + c3*controlled_area
+    return c1*distanceBetweenMeAndAdv + c3*controlled_area + c4*trapped_penalty
+    #+ c2*numWalls(chess_board)
 
 def flood_fill(chess_board, start_pos):
     """
